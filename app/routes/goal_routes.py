847886f354752a6e511a14a08c505149e3b1dc03
Malik Elmessiry <malik.messiry@gmail.com>
2025-05-08 13:31:43 +0200

    delete goal

--- app/routes/goal_routes.py
+++ app/routes/goal_routes.py
@@ -51,3 +51,12 @@ def update_goal(goal_id):
     db.session.commit()
 
     return Response(status=204, mimetype="application/json")
+
+# delete one goal
+@bp.delete("/<goal_id>")
+def delete_goal(goal_id):
+    goal = validate_model(Goal, goal_id)
+    db.session.delete(goal)
+    db.session.commit()
+
+    return Response(status=204, mimetype="application/json")
